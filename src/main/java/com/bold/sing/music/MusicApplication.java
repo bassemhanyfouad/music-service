34e24e81d3220e0f107fe2bb203080abeb78d8ba
bassemhanyfouad <bassemhanyfouad@gmail.com>
2020-07-27 21:08:22 +0200

    initial files

---
+++ src/main/java/com/bold/sing/music/MusicApplication.java
@@ -0,0 +1,13 @@
+package com.bold.sing.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class, args);
+    }
+
+}
